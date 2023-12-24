e27146fb1d059f8ed238f105d078bedce77f29d7
Lautaro Companys <lautarocompanys@gmail.com>
2023-12-23 21:06:10 -0300

    Proyecto final

--- main.js
+++ main.js
@@ -12,10 +12,13 @@ const equiposNBA = [
     { nombre: "Bucks", ciudad: "Milwuakee", ganados: 8, perdidos: 7, puntosPromedio: 112}
 ]
 
-const body = document.querySelector("body")
+
+
+const body = document.body
 const contenedorPrincipal = document.createElement("div")
 body.appendChild(contenedorPrincipal)
 
+// Posiciones
 const posicion = (equipoA, equipoB) => {
     if (equipoA.ganados !== equipoB.ganados) {
         return equipoB.ganados - equipoA.ganados
@@ -26,24 +29,29 @@ const posicion = (equipoA, equipoB) => {
 
 equiposNBA.sort(posicion)
 
+// Mostrar equipos
 function mostrarEquipos(equipos) {
-    contenedorPrincipal.innerHTML = ''
+
+    const equiposContainer = document.getElementById("equiposContainer")
+    equiposContainer.innerHTML = ''
 
     equipos.forEach((equipo) => {
         const plantilla = document.createElement("div")
+        plantilla.classList.add("plantillaEquipos")
 
         const ciudad = document.createElement("h2")
         ciudad.textContent = equipo.ciudad + " " + equipo.nombre
-        plantilla.appendChild(ciudad)
+        plantilla.appendChild(ciudad);
 
         const datos = document.createElement("p")
-        datos.textContent ="| Wins: " + equipo.ganados + " | Loses: " + equipo.perdidos + " | Puntos Promedio: " + equipo.puntosPromedio
+        datos.textContent = "| Wins: " + equipo.ganados + " | Loses: " + equipo.perdidos + " | Puntos Promedio: " + equipo.puntosPromedio
         plantilla.appendChild(datos)
 
-        contenedorPrincipal.appendChild(plantilla)
-    })
+        equiposContainer.appendChild(plantilla)
+    });
 }
 
+// Funcion playoffs
 function Playoff(equipos) {
     return equipos.filter((equipo) => equipo.ganados >= 7)
 }
@@ -51,7 +59,7 @@ function Playoff(equipos) {
 const equiposPlayoff = Playoff(equiposNBA)
 
 
-
+// Buscador
 const buscarBtn = document.getElementById("buscarBoton")
 buscarBtn.addEventListener("click", () => {
     const input = document.getElementById("buscarInput").value
@@ -69,7 +77,7 @@ buscarBtn.addEventListener("click", () => {
     }
 })
 
-
+// Botones playoffs y ver  tabla
 const playoffBtn = document.getElementById("playoff")
 playoffBtn.addEventListener("click", () => {
     mostrarEquipos(equiposPlayoff)
@@ -80,6 +88,7 @@ tablaBtn.addEventListener("click", () => {
     mostrarEquipos(equiposNBA)
 })
 
+//Historial
 function guardarHistorial(palabraClave) {
 
     const historial = JSON.parse(localStorage.getItem("historial")) || []
@@ -87,3 +96,97 @@ function guardarHistorial(palabraClave) {
 
     localStorage.setItem("historial", JSON.stringify(historial))
 }
+
+// Consumir API
+const url = "https://nba-stats-db.herokuapp.com/api/playerdata/topscorers/total/season/2023/"
+
+fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        const results = data.results
+        const playersContainer = document.getElementById("containerPlayers")
+
+    // Funciones de ordenamiento
+        const topPts = (a, b) => b.PTS - a.PTS
+        const topAst = (a, b) => b.AST - a.AST
+        const topBlk = (a, b) => b.BLK - a.BLK
+        const topStl = (a, b) => b.STL - a.STL
+
+    // Función para mostrar jugadores
+        const showPlayers = (sortedArray) => {
+        playersContainer.innerHTML = ''
+
+        sortedArray.forEach((player) => {
+        const playerElement = document.createElement("div")
+        playerElement.classList.add("playerElement")
+
+        const nameElement = document.createElement("div")
+        nameElement.textContent = `${player.player_name}`
+        nameElement.classList.add("nombreTitulo")
+
+        const minutesElement = document.createElement("p")
+        minutesElement.textContent = `Minutos jugados: ${player.minutes_played}`
+
+        const pointsElement = document.createElement("li")
+        pointsElement.textContent = `Puntos: ${player.PTS}`
+
+        const assistsElement = document.createElement("li")
+        assistsElement.textContent = `Asistencias: ${player.AST}`
+
+        const stealsElement = document.createElement("li")
+        stealsElement.textContent = `Robos: ${player.STL}`
+
+        const blkElement = document.createElement("li")
+        blkElement.textContent = `Tapas: ${player.BLK}`
+
+        playerElement.appendChild(nameElement)
+        playerElement.appendChild(minutesElement)
+        playerElement.appendChild(pointsElement)
+        playerElement.appendChild(assistsElement)
+        playerElement.appendChild(stealsElement)
+        playerElement.appendChild(blkElement)
+
+        playersContainer.appendChild(playerElement)
+        })
+    .catch ((error) => {
+        alert.error("A surgido un problema inesperado")
+    })};
+
+    //Botones ordenar top 20
+    const ptsBtn = document.getElementById("ptsBtn")
+    ptsBtn.addEventListener("click", () => {
+        const sortedResults = [...results].sort(topPts)
+        const top20Results = sortedResults.slice(0, 20)
+        showPlayers(top20Results);
+    });
+
+    const astBtn = document.getElementById("astBtn")
+    astBtn.addEventListener("click", () => {
+        const sortedResults = [...results].sort(topAst)
+        const top20Results = sortedResults.slice(0, 20)
+        showPlayers(top20Results)
+    });
+
+    const blkBtn = document.getElementById("blkBtn")
+    blkBtn.addEventListener("click", () => {
+        const sortedResults = [...results].sort(topBlk)
+        const top20Results = sortedResults.slice(0, 20)
+        showPlayers(top20Results)
+    });
+
+    const stlBtn = document.getElementById("stlBtn")
+    stlBtn.addEventListener("click", () => {
+        const sortedResults = [...results].sort(topStl)
+        const top20Results = sortedResults.slice(0, 20)
+        showPlayers(top20Results)
+    });
+
+    const allBtn = document.getElementById("allBtn")
+    allBtn.addEventListener("click", () => {
+        showPlayers(results)
+    })
+
+    showPlayers(results)
+    });
+
+    
